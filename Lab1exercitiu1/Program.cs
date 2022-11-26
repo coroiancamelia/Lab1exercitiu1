ab08b5fe72bbe385f3c7326880b5e608b74468b3
User <User@DESKTOP-VGD6AR3>
2022-11-26 10:16:44 +0200

    Add project files.

---
+++ Lab1exercitiu1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab1exercitiu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int laturaA = 5;
+
+            int laturaB = 6;
+
+            int laturaC = 7;
+
+            int volumulParalelipipedului = laturaA* laturaB* laturaC;
+
+            Console.WriteLine(volumulParalelipipedului);
+
+
+        }
+    }
+}
